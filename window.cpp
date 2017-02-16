9201f7a5562292aa07efcc752e504ac6a5a8bc14
Brad Neimann <brad.neimann@hotmail.com>
2017-02-16 17:01:54 +1100

    Change version in about box

--- window.cpp
+++ window.cpp
@@ -385,7 +385,7 @@ void Window::SaveLex()
 
 void Window::LaunchAboutBox()
 {
-    QMessageBox::about(this, "About exSCA", "<b>exSCA</b><br/>Version 2.0.0<br/>Copyright &copy; Brad Neimann 2017");
+    QMessageBox::about(this, "About exSCA", "<b>exSCA</b><br/>Version 2.0.1<br/>Copyright &copy; Brad Neimann 2017");
 }
 
 void Window::LaunchAboutQt()
