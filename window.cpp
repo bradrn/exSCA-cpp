19aff90a3fc86510e8f16422689b47fd8b046554
Brad Neimann <brad.neimann@hotmail.com>
2017-03-09 17:56:24 +1100

    Increment version

--- window.cpp
+++ window.cpp
@@ -395,7 +395,7 @@ void Window::SaveLex()
 
 void Window::LaunchAboutBox()
 {
-    QMessageBox::about(this, "About exSCA", "<b>exSCA</b><br/>Version 2.0.2<br/>Copyright &copy; Brad Neimann 2017");
+    QMessageBox::about(this, "About exSCA", "<b>exSCA</b><br/>Version 2.0.3<br/>Copyright &copy; Brad Neimann 2017");
 }
 
 void Window::LaunchAboutQt()
